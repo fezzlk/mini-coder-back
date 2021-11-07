3aa049321ab11d6eeaab15007c994f7bfa3f2317
fezzlk <fezzlk796@gmail.com>
2021-11-07 17:03:43 +0900

    fix route

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -10,17 +10,4 @@ export class AppController {
     console.log(query);
     return this.appService.getHello();
   }
-
-  @Get(':exam_id')
-  getExam(@Param() params): string {
-    console.log(params.exam_id);
-    return '整数 a, b が標準入力で与えられます。a + b を出力してください。';
-  }
-
-  // @Post('/answer')
-  // async postAns(@Body() answer) {
-  //   console.log(answer);
-  //   // this.answerService.checkAnswer();
-  //   return 'Received your answer. Please wait...'
-  // }
 }
